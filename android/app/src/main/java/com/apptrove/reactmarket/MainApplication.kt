0f8e734740a53087c23a2a8cacf53760d7a681a2
Satyam Jha <satyam@trackier.com>
2026-06-18 14:34:54 +0530

    Upgrade app version

--- android/app/src/main/java/com/apptrove/reactmarket/MainApplication.kt
+++ android/app/src/main/java/com/apptrove/reactmarket/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.apptrove.reactmarket
 
 import android.app.Application
+import a.BuildConfig
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
